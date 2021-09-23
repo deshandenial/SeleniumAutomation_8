2aa3c112ded4300257f4a6871ef5a39613995143
deshanraj <deshancse@gmail.com>
2021-09-23 16:24:21 +0530

    first commit

--- src/test/java/com/tbm/automation/Sample1.java
+++ src/test/java/com/tbm/automation/Sample1.java
@@ -14,4 +14,14 @@ public class Sample1 extends BaseClass
 		  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
 		  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
 	  }
+		
+		@Test
+		  public void test2() 
+		  {
+			
+			  driver.get("https://www.flipkart.com/");
+			  driver.findElement(By.xpath("//span[text()='Get access to your Orders, Wishlist and Recommendations']/following::input")).sendKeys("deshancse@gmail.com");
+			  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
+			  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
+		  }
 }
--- src/test/java/com/tbm/automation/Sample2.java
+++ src/test/java/com/tbm/automation/Sample2.java
@@ -16,7 +16,15 @@ public class Sample2 extends BaseClass
 		  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
 		  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
 	  }
-	  
+	  @Test
+	  public void test2() 
+	  {
+		
+		  driver.get("https://www.flipkart.com/");
+		  driver.findElement(By.xpath("//span[text()='Get access to your Orders, Wishlist and Recommendations']/following::input")).sendKeys("deshancse@gmail.com");
+		  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
+		  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
+	  }
 
 	  
 }
