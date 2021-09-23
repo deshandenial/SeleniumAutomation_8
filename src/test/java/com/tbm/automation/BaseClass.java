99d8d0325ac1301b721430b4cb2ae029e938d328
deshanraj <deshancse@gmail.com>
2021-09-23 16:30:10 +0530

    first commit

--- src/test/java/com/tbm/automation/BaseClass.java
+++ src/test/java/com/tbm/automation/BaseClass.java
@@ -3,9 +3,7 @@ package com.tbm.automation;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 public class BaseClass
 {
@@ -15,6 +13,7 @@ public class BaseClass
 	  {
 		  System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
 		  driver = new ChromeDriver();
+		  driver.get("https://www.flipkart.com/");
 	  }
 	  
 
--- src/test/java/com/tbm/automation/Sample1.java
+++ src/test/java/com/tbm/automation/Sample1.java
@@ -9,7 +9,7 @@ public class Sample1 extends BaseClass
 	  public void test1() 
 	  {
 		
-		  driver.get("https://www.flipkart.com/");
+		
 		  driver.findElement(By.xpath("//span[text()='Get access to your Orders, Wishlist and Recommendations']/following::input")).sendKeys("deshancse@gmail.com");
 		  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
 		  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
@@ -19,7 +19,7 @@ public class Sample1 extends BaseClass
 		  public void test2() 
 		  {
 			
-			  driver.get("https://www.flipkart.com/");
+			  
 			  driver.findElement(By.xpath("//span[text()='Get access to your Orders, Wishlist and Recommendations']/following::input")).sendKeys("deshancse@gmail.com");
 			  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
 			  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
