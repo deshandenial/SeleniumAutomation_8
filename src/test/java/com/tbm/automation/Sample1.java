88962d28125e5827c1d9f0d99bc30e7161bf6ca6
deshanraj <deshancse@gmail.com>
2021-09-23 16:13:45 +0530

    first commit

---
+++ src/test/java/com/tbm/automation/Sample1.java
@@ -0,0 +1,17 @@
+package com.tbm.automation;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Sample1 extends BaseClass
+{
+		@Test
+	  public void test1() 
+	  {
+		
+		  driver.get("https://www.flipkart.com/");
+		  driver.findElement(By.xpath("//span[text()='Get access to your Orders, Wishlist and Recommendations']/following::input")).sendKeys("deshancse@gmail.com");
+		  driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
+		  driver.findElement(By.xpath("//button[contains(@class,'_2KpZ6l _2HKlqd')]//span[1]")).click();
+	  }
+}
